4e56bf26bc1ba80bef5cca17dddbd9a25ecebae1
karimullin-ai <bolshakofw@gmail.com>
2022-03-25 18:32:43 +0500

    "Lvl2 Готов model- фигово, зип скачивает но я хз как , эксепшены не все"

--- src/main/java/com/example/FileSaver/FileController.java
+++ src/main/java/com/example/FileSaver/FileController.java
@@ -2,15 +2,16 @@ package com.example.FileSaver;
 
 
 import com.example.FileSaver.DTO.ChangeFileDto;
-import com.example.FileSaver.DTO.ModelDTO;
+
 import com.example.FileSaver.service.FileService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 import java.io.*;
@@ -26,8 +27,8 @@ public class FileController {
 
 
     @PostMapping("/upload")
-    public void upload(@RequestBody MultipartFile file) throws IOException {
-        fileService.uploadFile(file);
+    public FileData upload(@RequestBody MultipartFile file) throws IOException {
+        return fileService.uploadFile(file);
     }
 
     @GetMapping("/getList")
@@ -53,6 +54,14 @@ public class FileController {
     }
 
 
+    @GetMapping("/model")
+    public LinkedList<FileData> model(@RequestParam("name") String fileName,
+                                      @RequestParam("type") String fileType,
+                                      @RequestParam("from") long fromDate,
+                                      @RequestParam("till") long tillDate) {
+        return fileService.model(fileName, fileType, fromDate, tillDate);
+    }
+
     @GetMapping("/download/{id}")
     public HttpEntity<byte[]> download(@PathVariable UUID id) {
         return fileService.download(id);
@@ -61,16 +70,14 @@ public class FileController {
 
     @GetMapping("/downloadZip")
     public HttpEntity<byte[]> downloadZip(@RequestParam UUID[] ids) throws IOException {
+
         byte[] body = fileService.downloadZip(ids);
         HttpHeaders header = new HttpHeaders();
         header.set("Content-Disposition", "attachment; filename=" + "files.zip");
-
         header.setContentLength(body.length);
+        String downloadZipUri = ServletUriComponentsBuilder.fromCurrentServletMapping().path("/downloadZip/").path(Arrays.toString(ids)).toUriString();
         return new HttpEntity<>(body, header);
     }
 
-    @GetMapping("/model")
-    public ArrayList<FileData> model(@RequestBody ModelDTO modelDTO) {
-        return fileService.model(modelDTO);
-    }
+
 }
--- src/main/java/com/example/FileSaver/service/FileService.java
+++ src/main/java/com/example/FileSaver/service/FileService.java
@@ -1,8 +1,8 @@
 package com.example.FileSaver.service;
 
 import com.example.FileSaver.DTO.ChangeFileDto;
-import com.example.FileSaver.DTO.ModelDTO;
 import com.example.FileSaver.FileData;
+import com.example.FileSaver.exceptions.FileNotFoundException;
 import com.example.FileSaver.exceptions.WrongFileSizeException;
 import com.example.FileSaver.exceptions.WrongFileTypeException;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +24,7 @@ import java.util.zip.ZipOutputStream;
 public class FileService {
 
 
-    private static final List<String> CONTENT_TYPES = Arrays.asList("image/png", "image/jpeg", "image/gif", "text/plain", "pdf/application", "");
+    private static final List<String> CONTENT_TYPES = Arrays.asList("image/png", "image/jpeg", "image/gif", "text/plain", "pdf/application");
     private static final long MAX_SIZE = 15 * 1024 * 1024;
     private LinkedList<FileData> list = new LinkedList<>();
 
@@ -33,7 +33,7 @@ public class FileService {
         this.list = list;
     }
 
-    public void uploadFile(MultipartFile file) throws IOException {
+    public FileData uploadFile(MultipartFile file) throws IOException {
         if (!CONTENT_TYPES.contains((file.getContentType()))) {
             throw new WrongFileTypeException("Wrong file type");
         } else if (!(file.getSize() < MAX_SIZE)) {
@@ -51,6 +51,7 @@ public class FileService {
         fileData.setFileDownloadUri(ServletUriComponentsBuilder.fromCurrentServletMapping().path("/download/").path(fileData.getId().toString()).toUriString());
 
         list.add(fileData);
+        return fileData;
     }
 
 
@@ -88,8 +89,13 @@ public class FileService {
     }
 
     public HttpEntity<byte[]> download(UUID id) {
+
+
         for (FileData fileData : list) {
-            if (fileData.getId().equals(id)) {
+            if (!fileData.getId().equals(id)) {
+                throw new FileNotFoundException("File not fount");
+            }
+            else if (fileData.getId().equals(id)) {
                 byte[] body = fileData.getBytes();
                 HttpHeaders header = new HttpHeaders();
                 header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileData.getFileName());
@@ -101,24 +107,6 @@ public class FileService {
     }
 
 
-
-
-    public ArrayList<FileData> model(ModelDTO modelDTO) {
-        ArrayList<FileData> finalList = new ArrayList<>();
-        for(FileData fileData:list){
-            if(modelDTO.getFileName()!= null && fileData.getFileName().contains(modelDTO.getFileName())){
-                finalList.add(fileData);
-            }
-            if(modelDTO.getFileType()!=null && fileData.getFileType().contains(modelDTO.getFileType())){
-                if(!finalList.contains(fileData)){
-                    finalList.add(fileData);
-                }
-            }
-        }
-        return finalList;
-    }
-
-
     public byte[] downloadZip(UUID[] uuids) throws IOException {
         for (FileData fileData : list) {
             if (Arrays.stream(uuids).toList().contains(fileData.getId())) {
@@ -127,6 +115,7 @@ public class FileService {
                 zos.putNextEntry(new ZipEntry(fileData.getFileName()));
                 zos.write(fileData.getBytes());
                 zos.closeEntry();
+                zos.close();
                 return bos.toByteArray();
             }
         }
@@ -135,5 +124,30 @@ public class FileService {
         return new byte[0];
     }
 
+    public LinkedList<FileData> model(String fileName,String fileType,Long fromDate,Long tillDate){
+        LinkedList<FileData> spisok = new LinkedList<>();
+        for(FileData fileData:list){
+            if (fileName!=null && !fileName.isEmpty() && fileData.getFileName().contains(fileName)){
+                spisok.add(fileData);
+            }
+            if(fileType!=null && !fileType.isEmpty() && fileData.getFileType().contains(fileType)){
+                if(!spisok.contains(fileData)){
+                    spisok.add(fileData);
+                }
+            }
+            if(fromDate!=null && fileData.getChange()!= null && fileData.getChange().getTime() > fromDate){
+                if(!spisok.contains(fileData)){
+                    spisok.add(fileData);
+                }
+            }
+            if(tillDate!=null && fileData.getChange()!= null && fileData.getChange().getTime() < tillDate){
+                if(!spisok.contains(fileData)){
+                    spisok.add(fileData);
+                }
+            }
+        }
+        return spisok;
+
+    }
 
 }
