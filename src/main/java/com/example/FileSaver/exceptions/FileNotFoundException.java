d0f5aff5378725c18683789e76cf272c117c890d
karimullin-ai <bolshakofw@gmail.com>
2022-03-25 18:32:49 +0500

    "Lvl2 Готов model- фигово, зип скачивает но я хз как , эксепшены не все"

---
+++ src/main/java/com/example/FileSaver/exceptions/FileNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.FileSaver.exceptions;
+
+public class FileNotFoundException extends RuntimeException {
+    public FileNotFoundException(String message){
+        super(message);
+    }
+}
